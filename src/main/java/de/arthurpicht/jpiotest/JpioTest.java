3e7f3248c698c8cd83c077202d4aea8e5c37ab11
arthurpicht <info@arthurpicht.de>
2020-10-30 10:11:03 +0100

    gradle wrapper v6.7, jdk toolchain v11

---
+++ src/main/java/de/arthurpicht/jpiotest/JpioTest.java
@@ -0,0 +1,8 @@
+package de.arthurpicht.jpiotest;
+
+public class JpioTest {
+
+    public static void main(String[] args) {
+        System.out.println("JPIO Test.");
+    }
+}
---
+++ src/test/java/de/arthurpicht/jpiotest/JpioTestTest.java
@@ -0,0 +1,15 @@
+package de.arthurpicht.jpiotest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JpioTestTest {
+
+    @Test
+    void testDummy() {
+
+        System.out.println("Some test stub");
+
+    }
+}
