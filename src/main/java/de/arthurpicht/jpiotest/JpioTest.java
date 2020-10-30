928c47704c197bdd7d9e1586070ab87d1011ea38
arthurpicht <info@arthurpicht.de>
2020-10-30 11:10:42 +0100

    code requires JDK 11 to compile

--- src/main/java/de/arthurpicht/jpiotest/JpioTest.java
+++ src/main/java/de/arthurpicht/jpiotest/JpioTest.java
@@ -1,6 +1,23 @@
 package de.arthurpicht.jpiotest;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class JpioTest {
+
+    /**
+     * requires JDK 11
+     */
+    public static void varLambda() {
+
+        List<String> list = Arrays.asList("a", "b", "c");
+
+        // keyword 'var' in lambda requires JDK 11
+        String result = list.stream().map((var x) -> x.toUpperCase()).collect(Collectors.joining(","));
+        System.out.println();
+
+    }
 
     public static void main(String[] args) {
         System.out.println("JPIO Test.");
