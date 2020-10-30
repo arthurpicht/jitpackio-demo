32edb7e38030b1f18aeb522117d49bc1ba10d76e
arthurpicht <info@arthurpicht.de>
2020-10-30 12:27:05 +0100

    requires JDK 14 to compile

--- src/main/java/de/arthurpicht/jpiotest/JpioTest.java
+++ src/main/java/de/arthurpicht/jpiotest/JpioTest.java
@@ -10,12 +10,26 @@ public class JpioTest {
      * requires JDK 11
      */
     public static void varLambda() {
-
         List<String> list = Arrays.asList("a", "b", "c");
-
         // keyword 'var' in lambda requires JDK 11
         String result = list.stream().map((var x) -> x.toUpperCase()).collect(Collectors.joining(","));
         System.out.println();
+    }
+
+    /**
+     * requires JDK 14, preview in JDK 13
+     */
+    public static String switchYield(int number) {
+        return switch (number) {
+            case 1, 2:
+                yield "one or two";
+            case 3:
+                yield "three";
+            case 4, 5, 6:
+                yield "four or five or six";
+            default:
+                yield "unknown";
+        };
 
     }
 
